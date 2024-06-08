2bac20072e54230c712699bb85ebff73b2645753
fakerepic <yanglang21@mails.tsinghua.edu.cn>
2024-06-08 13:45:24 +0800

    feat: chat api

--- ai_service/app.py
+++ ai_service/app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from typing import Optional, List
 from llama_index.core import VectorStoreIndex
+from llama_index.core.llms import ChatMessage, MessageRole
 from llama_index.core.vector_stores.types import VectorStoreQuery
 from pydantic import BaseModel, Field
 
@@ -36,6 +37,28 @@ class QuerySearch(BaseModel):
     similarity_top_k: Optional[int] = Field(default=1, ge=1, le=5)
 
 
+class ChatSourceModal(BaseModel):
+    id: str
+    title: Optional[str]
+    score: Optional[float]
+
+
+class ResponseChat(BaseModel):
+    content: str
+    sources: List[ChatSourceModal]
+
+
+class ChatMessageModel(BaseModel):
+    role: MessageRole
+    content: str
+
+
+class QueryChat(BaseModel):
+    query: str
+    similarity_top_k: Optional[int] = Field(default=1, ge=1, le=5)
+    history: Optional[List[ChatMessageModel]] = None
+
+
 @app.post("/ai/question", response_model=ResponseQuestion, status_code=200)
 def question(user_id: str, query: QuerySearch):
     rag = RAG(userid=user_id)
@@ -53,6 +76,36 @@ def question(user_id: str, query: QuerySearch):
         sources=[node.metadata.get("id") for node in response.source_nodes],  # type: ignore
     )
     return response_object
+
+
+@app.post("/ai/chat", response_model=ResponseChat, status_code=200)
+def chat(user_id: str, query: QueryChat):
+    rag = RAG(userid=user_id)
+    vector_store = get_vectorstore()
+    index = VectorStoreIndex.from_vector_store(vector_store)
+    chat_engine = index.as_chat_engine(
+        similarity_top_k=query.similarity_top_k,
+        verbose=True,
+        filters=rag.user_selector,
+    )
+    response = chat_engine.chat(
+        query.query,
+        chat_history=[
+            ChatMessage.from_str(content=chat.content, role=chat.role)
+            for chat in query.history or []
+        ],
+    )
+    return ResponseChat(
+        content=str(response).strip(),
+        sources=[
+            ChatSourceModal(
+                id=node.metadata.get("id"),  # type: ignore
+                title=node.metadata.get("title"),
+                score= node.get_score(),
+            )
+            for node in response.source_nodes  # type: ignore
+        ],
+    )
 
 
 @app.post("/ai/search", response_model=ResponseSearch, status_code=200)
